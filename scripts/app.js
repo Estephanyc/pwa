211722500788a8aebde54d02ce9f7c800dde7969
estephanyc <maitilydevidasi@gmail.com>
2019-02-07 17:43:36 -0300

    notificaciones

--- scripts/app.js
+++ scripts/app.js
@@ -305,12 +305,123 @@
   // TODO uncomment line below to test app with fake data
   app.updateForecastCard(initialWeatherForecast);
 
-  // TODO add startup code here
+  function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+  
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+  
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
 
+  // TODO add startup code here
+  const pushButton = document.querySelector('.js-push-btn');
+
+  let isSubscribed = false;
+  let swRegistration = null;
   // TODO add service worker code here
-  if ('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
+
     navigator.serviceWorker
              .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
+             .then(function(swReg) {
+              console.log('Service Worker is registered', swReg);
+              swRegistration = swReg;
+              
+              initialiseUI();
+
+            })             .catch(function(error) {
+              console.error('Service Worker Error', error);
+            });
+  }
+  else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
+  }
+  const applicationServerPublicKey = 'BKYIucaKV5vlA_6JlxxhEKnzAM2QP-gTSp5BztoQgK47YmJsh6E5i1DKmZnj0oMMvbMU5VJw6sIct2ta9xPT10o';
+  function initialiseUI() {
+    // Set the initial subscription value
+    pushButton.addEventListener('click', function() {
+      pushButton.disabled = true;
+      if (isSubscribed) {
+        // TODO: Unsubscribe user
+      } else {
+        subscribeUser();
+      }
+    });
+    
+   // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    updateSubscriptionOnServer(subscription);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed.');
+    } else {
+      console.log('User is NOT subscribed.');
+    }
+
+    updateBtn();
+  });
+  }
+  function updateBtn() {
+    if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+  }
+  function subscribeUser() {
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    swRegistration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function(subscription) {
+      console.log('User is subscribed:', subscription);
+  
+      updateSubscriptionOnServer(subscription);
+  
+      isSubscribed = true;
+  
+      updateBtn();
+    })
+    .catch(function(err) {
+      console.log('Failed to subscribe the user: ', err);
+      updateBtn();
+    });
+  }
+
+  function updateSubscriptionOnServer(subscription) {
+    // TODO: Send subscription to application server
+  
+    const subscriptionJson = document.querySelector('.js-subscription-json');
+    const subscriptionDetails =
+      document.querySelector('.js-subscription-details');
+  
+    if (subscription) {
+      subscriptionJson.textContent = JSON.stringify(subscription);
+      subscriptionDetails.classList.remove('is-invisible');
+    } else {
+      subscriptionDetails.classList.add('is-invisible');
+    }
   }
 })();
